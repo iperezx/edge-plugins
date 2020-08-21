30fe841e0b4fc89067d1e9c1ad1a1c42c39b41d4
iperezx <i3perez@sdsc.edu>
2020-08-21 16:11:49 -0700

    Helper functions for hpwren api and random number generation for camera site

--- plugin-smokedetect/src/hpwren.py
+++ plugin-smokedetect/src/hpwren.py
@@ -14,3 +14,22 @@ class cameras:
         imageURL = hpwrenCamsAtSite[cameraID][0]["image"]
         description = hpwrenCamsAtSite[cameraID][0]["description"]
         return imageURL,description
+
+    def getNumOfCamerasAtSite(self,siteID=0):
+        hpwrenCams = self.hpwrenCams
+        hpwrenCamsF = hpwrenCams["features"]
+        hpwrenCamsAtSite = hpwrenCamsF[siteID]["properties"]["latest-images"]
+        return len(hpwrenCamsAtSite)
+
+    def getNumOfSites(self):
+        hpwrenCams = self.hpwrenCams
+        hpwrenCamsF = hpwrenCams["features"]
+        return len(hpwrenCamsF)
+
+    def getSiteName(self,siteID=0):
+        hpwrenCams = self.hpwrenCams
+        hpwrenCamsF = hpwrenCams["features"]
+        siteName = hpwrenCamsF[siteID]['properties']['description']['name']
+        return siteName
+        
+
--- plugin-smokedetect/src/main.py
+++ plugin-smokedetect/src/main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import inference,hpwren,composableSys
 import tflite_runtime.interpreter as tflite
-import time,datetime,os,sys,subprocess
+import time,datetime,os,sys,subprocess,random
 #import waggle.plugin,logging,requests
 from distutils.util import strtobool
+
 
 object = 'model.tflite'
 directory = '/data/model/'
@@ -12,12 +13,11 @@ modelPath = os.path.join(directory,object)
 hpwrenUrl = "https://firemap.sdsc.edu/pylaski/\
 stations?camera=only&selection=\
 boundingBox&minLat=0&maxLat=90&minLon=-180&maxLon=0"
-cameraID=0
-siteID=0
 camObj = hpwren.cameras(hpwrenUrl)
-
+numWorkingSites = 40
+siteID = random.randrange(numWorkingSites)
+numCamerasAtSite = camObj.getNumOfCamerasAtSite(siteID)
 serverName = 'HPWREN Camera'
-imageURL,description = camObj.getImageURL(cameraID,siteID)
 
 #triger setup
 urlWindEvents = 'https://wind.events/auth'
@@ -29,27 +29,33 @@ counter = 0
 # plugin = waggle.plugin.Plugin()
 print('Starting smoke detection inferencing')
 while True:
-    testObj = inference.FireImage()
     print('Get image from ' + serverName)
-    print("Image url: " + imageURL)
-    print("Description: " + description)
-    testObj.urlToImage(imageURL)
-    interpreter = tflite.Interpreter(model_path=modelPath)
-    interpreter.allocate_tensors()
-    print('Perform an inference based on trainned model')
-    result,percent = testObj.inference(interpreter)
-    print(result)
-    currentDT = str(datetime.datetime.now())
+    print('Site ID: ' + str(siteID))
+    print('Site Name: ' + camObj.getSiteName(siteID))
+    print('Number of Cameras on site for inference: ' + str(numCamerasAtSite))
+    for cameraID in range(numCamerasAtSite):
+        testObj = inference.FireImage()
+        imageURL,description = camObj.getImageURL(cameraID,siteID)
+        print("Image url: " + imageURL)
+        print("Description: " + description)
+        testObj.urlToImage(imageURL)
+        interpreter = tflite.Interpreter(model_path=modelPath)
+        interpreter.allocate_tensors()
+        print('Perform an inference based on trainned model')
+        result,percent = testObj.inference(interpreter)
+        print(result)
+        currentDT = str(datetime.datetime.now())
 
-    if (counter == 0 or ("Fire" in result and percent > 0.75)):
-        print('Trigger: Launch Ensemble')
-        ensembleLatLongList = trigger.getEnsembleLatLongList([32.848132, -116.805901])
-        params = trigger.getDefaultParams()
-        for latLong in ensembleLatLongList:
-            params['ignition']['point'] =  latLong
-            params['webhook']['request_id'] = str(counter)
-            counter += 1
-            returnStatus = trigger.launchFarsiteModel(params)
+        if (counter == 0 or ("Fire" in result and percent > 0.75)):
+            print('Trigger: Launch Ensemble')
+            ensembleLatLongList = trigger.getEnsembleLatLongList([32.848132, -116.805901])
+            params = trigger.getDefaultParams()
+            for latLong in ensembleLatLongList:
+                params['ignition']['point'] =  latLong
+                params['webhook']['request_id'] = str(counter)
+                counter += 1
+                returnStatus = trigger.launchFarsiteModel(params)
 
+        print(' ')
+        time.sleep(5)
     print(' ')
-    time.sleep(5)
