0cae74a4213d9f0d7ec86da487836c8e12edd41f
iperezx <i3perez@sdsc.edu>
2021-04-28 15:26:16 -0700

    Add try-catch for hpwren request with http error printing.

--- plugin-smokedetect/src/hpwren.py
+++ plugin-smokedetect/src/hpwren.py
@@ -1,11 +1,25 @@
 import os
 import requests
+from requests.exceptions import HTTPError
 
 class cameras:
     def __init__(self,hpwrenUrl):
         self.hpwrenUrl = hpwrenUrl
-        self.requestData = requests.get(self.hpwrenUrl)
+        self.setHPWRENCamsData()
+
+    def setHPWRENCamsData(self):
+        try:
+            self.requestData = requests.get(self.hpwrenUrl)
+            self.requestData.raise_for_status()
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # Python 3.6
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+        else:
+            print('Success!')
+
         self.hpwrenCams = self.requestData.json()
+
 
     def getImageURL(self, cameraID=0,siteID=0):
         hpwrenCams = self.hpwrenCams
